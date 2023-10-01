1f0d95fa6383acb1afdb772f091cec1025f27dab
Hoanghaine <dohoanghai08062003@gmail.com>
2023-10-01 17:44:48 +0700

    header footer dealerTab completed

---
+++ fontend/assets/js/app.js
@@ -0,0 +1,28 @@
+
+function loadHTML(id, filename) {
+  console.log(`div id: ${id}, filename: ${filename}`);
+  let xhttp;
+  let element = document.getElementById(id);
+  let file = filename;
+  if (file) {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          element.innerHTML = this.responseText;
+        }
+        if (this.status == 400) {
+          element.innerHTML = "<h1>Page not found.</h1>";
+        }
+      }
+
+    }
+    xhttp.open("GET", `tabs/${file}`, true);
+    xhttp.send();
+    return;
+  }
+}
+loadHTML("tab", "mainTab.html");
+
+
+
