28b47295d1545044c04b19c528ff24e0108911b2
anhbaonb24 <anhbaonb24@gmail.com>
2023-10-06 16:12:25 +0700

    no message

--- fontend/assets/js/bao.js
+++ fontend/assets/js/bao.js
@@ -1,18 +1,15 @@
-const images = ['http://daugoidau.themevivu.site/wp-content/uploads/2022/06/banner-naciva-270622.jpg', 'http://daugoidau.themevivu.site/wp-content/uploads/2022/06/SLIDER1.jpg', ]; 
+
+
+const image = document.getElementById('image');
+const imageSources = [
+    'http://daugoidau.themevivu.site/wp-content/uploads/2022/06/banner-naciva-270622.jpg', 'http://daugoidau.themevivu.site/wp-content/uploads/2022/06/SLIDER1.jpg',
+];
 
 let currentImageIndex = 0;
-const imageContainer = document.querySelector('.home_title');
-const imageElement = imageContainer.querySelector('img');
-console.log("bao")
+
 function changeImage() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    const nextImageSrc = images[currentImageIndex];
-    imageElement.style.opacity = 0; // Ẩn ảnh hiện tại
-    console.log("bao")
-    setTimeout(() => {
-        imageElement.src = nextImageSrc; // Đổi ảnh mới
-        imageElement.style.opacity = 1; // Hiển thị ảnh mới
-    }, 500); // Thời gian chờ trước khi thay đổi ảnh (0.5 giây)
+    image.src = imageSources[currentImageIndex];
+    currentImageIndex = (currentImageIndex + 1) % imageSources.length;
 }
 
 setInterval(changeImage, 2000);
