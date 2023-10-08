7c171d4b0fa84fd0f5bed9187609d4a5f47b0832
Hoanghaine <dohoanghai08062003@gmail.com>
2023-10-08 10:08:10 +0700

    nothing

--- fontend/assets/js/app.js
+++ fontend/assets/js/app.js
@@ -1,4 +1,3 @@
-
 function loadHTML(id, filename) {
   console.log(`div id: ${id}, filename: ${filename}`);
   let xhttp;
@@ -10,19 +9,37 @@ function loadHTML(id, filename) {
       if (this.readyState == 4) {
         if (this.status == 200) {
           element.innerHTML = this.responseText;
+          if (filename === 'homeTab.html') {
+            changeImage();
+          }
         }
         if (this.status == 400) {
           element.innerHTML = "<h1>Page not found.</h1>";
         }
       }
-
     }
     xhttp.open("GET", `tabs/${file}`, true);
     xhttp.send();
     return;
   }
 }
-loadHTML("tab", "homeTab.html");
+function changeImage() {
+  const image = document.getElementById('image');
+  console.log(image);
+  console.log("heh");
+  const imageSources = [
+    'http://daugoidau.themevivu.site/wp-content/uploads/2022/06/banner-naciva-270622.jpg', 'http://daugoidau.themevivu.site/wp-content/uploads/2022/06/SLIDER1.jpg',
+  ];
 
+  let currentImageIndex = 0;
 
+  function changeImage() {
+    image.src = imageSources[currentImageIndex];
+    currentImageIndex = (currentImageIndex + 1) % imageSources.length;
+  }
 
+  setInterval(changeImage, 2000);
+}
+
+// Call the loadHTML function with the callback to execute changeImage
+loadHTML("tab", "homeTab.html", changeImage);
