8fb0a5dd4d7bbac249bb51f1ad739bf89b1a3608
Thịnh Trần <thinhtran3803@gmail.com>
2023-10-13 13:26:26 +0700

    ..

--- fontend/assets/js/app.js
+++ fontend/assets/js/app.js
@@ -1,39 +1,35 @@
-
 console.log("app");
-loadHTML('tab','homeTab.html')
+loadHTML("tab", "homeTab.html");
 function loadHTML(id, filename) {
-    let xhttp;
-    let element = document.getElementById(id);
-    let file = filename;
-    if (file) {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    element.innerHTML = this.responseText;
-                    if (filename === 'homeTab.html') {    
-                        autoChangeImage();
-                    }
-                    if (filename === 'productTab.html') {    
-                        sortProductByPrice();
-                    }
-                    if (filename === 'tipTab.html') {    
-                        changeImageProduct();
-                    }
+	let xhttp;
+	let element = document.getElementById(id);
+	let file = filename;
+	if (file) {
+		xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4) {
+				if (this.status == 200) {
+					element.innerHTML = this.responseText;
+					if (filename === "homeTab.html") {
+						autoChangeImage();
+					}
+					if (filename === "productTab.html") {
+						sortProductByPrice();
+					}
+					if (filename === "tipTab.html") {
+						changeImageProduct();
+					}
+				}
+				if (this.status == 400) {
+					element.innerHTML = "<h1>Page not found.</h1>";
+				}
+			}
+		};
 
-                }
-                if (this.status == 400) {
-                    element.innerHTML = "<h1>Page not found.</h1>";
-                }
-            }
-            }
-            xhttp.open("GET", `tabs/${file}`, true);
-            xhttp.send();
-            return;
-    }
+		xhttp.open("GET", `tabs/${file}`, true);
+		xhttp.send();
+		return;
+	}
 }
 
-
 // ========================================================bao==============================
-
-
