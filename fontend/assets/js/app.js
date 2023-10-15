7f6d2a020ad4ccaead654dd85aa1720fd596a8ff
Thịnh Trần <thinhtran3803@gmail.com>
2023-10-15 18:13:50 +0700

    js selected tab

--- fontend/assets/js/app.js
+++ fontend/assets/js/app.js
@@ -1,38 +1,41 @@
-
 console.log("app");
 
-loadHTML('tab','homeTab.html')
+loadHTML("tab", "homeTab.html");
 function loadHTML(id, filename) {
-    let xhttp;
-    let element = document.getElementById(id);
-    let file = filename;
-    if (file) {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    element.innerHTML = this.responseText;
-                    console.log('changetab')
-                    if (filename === 'homeTab.html') {   
-                        console.log('hometab');
-                        autoChangeImage();
-                    }
-                    if (filename === 'productTab.html') {    
-                        sortProductByPrice();
-                    }
-
-                    
-                }
-                if (this.status == 400) {
-                    element.innerHTML = "<h1>Page not found.</h1>";
-                }
-            }
-            }
-            xhttp.open("GET", `tabs/${file}`, true);
-            xhttp.send();
-            return;
-    }
+	let xhttp;
+	let element = document.getElementById(id);
+	let file = filename;
+	if (file) {
+		xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4) {
+				if (this.status == 200) {
+					element.innerHTML = this.responseText;
+					console.log("changetab");
+					if (filename === "homeTab.html") {
+						console.log("hometab");
+						autoChangeImage();
+					}
+					if (filename === "productTab.html") {
+						sortProductByPrice();
+					}
+				}
+				if (this.status == 400) {
+					element.innerHTML = "<h1>Page not found.</h1>";
+				}
+			}
+		};
+		xhttp.open("GET", `tabs/${file}`, true);
+		xhttp.send();
+		return;
+	}
 }
+// sleceted tab
+const menuItems = document.querySelectorAll(".header__nav ul li a");
 
-
-
+menuItems.forEach(menuItem => {
+	menuItem.addEventListener("click", () => {
+		menuItems.forEach(item => item.classList.remove("selected"));
+		menuItem.classList.add("selected");
+	});
+});
