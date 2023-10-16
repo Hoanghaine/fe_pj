67c6b1559e71201be3439b466ede1d702c8d50cd
Hoanghaine <dohoanghai08062003@gmail.com>
2023-10-16 18:03:20 +0700

    90%

--- fontend/assets/js/app.js
+++ fontend/assets/js/app.js
@@ -2,7 +2,6 @@ console.log("app");
 
 loadHTML("tab", "homeTab.html");
 function loadHTML(id, filename) {
-<<<<<<< HEAD
     let xhttp;
     let element = document.getElementById(id);
     let file = filename;
@@ -37,40 +36,6 @@ function loadHTML(id, filename) {
             xhttp.send();
             return;
     }
-=======
-	let xhttp;
-	let element = document.getElementById(id);
-	let file = filename;
-	if (file) {
-		xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function () {
-			if (this.readyState == 4) {
-				if (this.status == 200) {
-					element.innerHTML = this.responseText;
-					console.log("changetab");
-					if (filename !== "cartTab.html" && filename !== "billTab.html") {
-						activeShowHideCartDropdown();
-					} else {
-						offShowHideCartDropdown();
-					}
-					if (filename === "homeTab.html") {
-						console.log("hometab");
-						autoChangeImage();
-					}
-					if (filename === "productTab.html") {
-						sortProductByPrice();
-					}
-				}
-				if (this.status == 400) {
-					element.innerHTML = "<h1>Page not found.</h1>";
-				}
-			}
-		};
-		xhttp.open("GET", `tabs/${file}`, true);
-		xhttp.send();
-		return;
-	}
->>>>>>> 9cd624737eb5845f2fdece22a3a2e60be2d0cf8b
 }
 //selected tab
 const menuItems = document.querySelectorAll(".header__nav ul li a");
